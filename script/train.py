1feb19bbe0e6d39268fd9224553b3574712c12d3
SebastienBoisard <sebastien.boisard@gmail.com>
2017-06-24 16:28:31 +0200

    Clean the train script:   - optimize the parameters in the loggin functions   - add some references for Tensorflow functions   - specify the proper exception for tf.load_variable   - simplify the subprocess call parameters

--- script/train.py
+++ script/train.py
@@ -1,17 +1,13 @@
 import tensorflow as tf
 import subprocess
 import logging
-import os
+
 
 MAX_STEPS = 300000
 STEPS_PER_VALIDATION = 1000
 STEPS_PER_CHECKPOINT = 20000
 TEST_THRESHOLD = 200000
 
-train_params = {
-    "--steps_per_validation": STEPS_PER_VALIDATION,
-    "--steps_per_checkpoint": STEPS_PER_CHECKPOINT,
-}
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG,
@@ -19,23 +15,24 @@ if __name__ == "__main__":
                         datefmt='%b %d %H:%M')
 
     try:
-        global_step = tf.contrib.framework.load_variable("model", "global_step")
-    except:
+        # Returns a Tensor with the contents of the given variable in the checkpoint.
+        # Doc: https://www.tensorflow.org/api_docs/python/tf/contrib/framework/load_variable
+        global_step = tf.contrib.framework.load_variable(checkpoint_dir="model", name="global_step")
+    except tf.errors.NotFoundError:
         global_step = 0
 
-    logging.info("Training starts with global_step={}. ".format(global_step))
+    logging.info("Training starts with global_step=%d", global_step)
 
     while global_step < MAX_STEPS:
         terminate_step = max(global_step + STEPS_PER_CHECKPOINT, TEST_THRESHOLD)
 
-        logging.info("Train from {} to {}. ".format(
-            global_step, terminate_step))
+        logging.info("Train from %d to %d", global_step, terminate_step)
 
         proc = ["python3", "src/summarization.py",
-            "--max_iter", str(terminate_step)]
-        for key, val in train_params.items():
-            proc.append(key)
-            proc.append(str(val))
+                "--max_iter", str(terminate_step),
+                "--steps_per_validation", str(STEPS_PER_VALIDATION),
+                "--steps_per_checkpoint", str(STEPS_PER_CHECKPOINT)]
+
         subprocess.call(proc)
 
         global_step = terminate_step
